55d7938d42eced836c40452608736e6600b90768
Mason <mason@Masons-MacBook-Air-2.local>
2023-07-18 21:52:58 +0100

    can signin with email

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-
 import { NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
 import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
 
 export default function App() {
 
@@ -10,8 +10,9 @@ export default function App() {
   
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name='Home' component={SignUp} />
+      <Stack.Navigator initialRouteName='SignIn'>
+        <Stack.Screen name='SignIn' component={SignIn}/>
+        <Stack.Screen name='SignUp' component={SignUp} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- pages/SignUp.js
+++ pages/SignUp.js
@@ -3,37 +3,47 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 import { Text, View, TextInput, Button } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { auth } from '../firebaseConfig';
+import { addUser } from "../functions";
 
 const SignUp = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [first, setFirst] = useState("");
     const [last, setLast] = useState("")
+    const [state, setState] = useState(false)
     function handlePress() {
-      createUserWithEmailAndPassword(auth, email, password)
-        .then((userCred) => {
-          const user = userCred.user;
-          console.log(user);
-        })
-        .catch((err) => {
-          console.log(err.message);
-        });
+        if (email != '' && password != "" && first !== "" && last !== "") {
+            createUserWithEmailAndPassword(auth, email, password)
+                .then((userCred) => {
+                    const user = userCred.user;
+                    return user;
+                })
+                .then(user => {
+                    addUser(user.uid, first, last);
+                })
+                .catch((err) => {
+                    console.log(err.message);
+                });
+        } else {
+            setState(true)
+        }
     }
     return (
       <View>
         <Text>Email</Text>
         <TextInput onChangeText={setEmail} value={email} placeholder="email" />
-        <Text>password</Text>
+        <Text>Password</Text>
         <TextInput
           onChangeText={setPassword}
           value={password}
-          placeholder="password"
+          placeholder="password..."
           secureTextEntry={true}
         />
         <Text>First Name:</Text>
         <TextInput onChangeText={setFirst} value={first} placeholder="first name..." />
         <Text>Last name:</Text>
-        <TextInput onChangeText={setLast} value={last} placeholder="last name..." />
+            <TextInput onChangeText={setLast} value={last} placeholder="last name..." />
+            {state?<Text>fill in all fields</Text> :null}
         <Button onPress={handlePress} title="Submit" />
         <StatusBar />
       </View>
